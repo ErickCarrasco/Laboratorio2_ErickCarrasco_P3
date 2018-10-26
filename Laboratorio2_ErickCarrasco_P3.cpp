8fa60039026c2b7ac385b35dd56ed31c6be05c69
Erick Carrasco <erick.e.carrasco88@gmail.com>
2018-10-26 15:26:44 -0600

    Se ha terminado el ejercicio 1 y se esta editando el ejercicio 2

--- Laboratorio2_ErickCarrasco_P3.cpp
+++ Laboratorio2_ErickCarrasco_P3.cpp
@@ -9,9 +9,10 @@ using std::endl;
 #include <string>
 //using std::to_string;
 //using std::stoi;
-
+using std::string;
 #include <ctime>
 
+//FUNCIONES PARA EJERCICIO 1
 int* createArray(int);
 
 void freeArray(int*);
@@ -19,6 +20,16 @@ void freeArray(int*);
 void initArray(int*, int);
 
 void printArray(int*, int);
+
+//FUNCIONES PARA EJERCICIO 2
+
+char* createCharArray(int);
+
+void initCharArray(char*, int);
+
+void printCharArray(char*, int);
+
+void freeCharArray(char*);
 
 int main(){
 	
@@ -45,26 +56,105 @@ int main(){
 				int* arreglo = NULL;
 				arreglo = createArray(size);
 				initArray(arreglo, size);
-				//printArray(arreglo, size);
+				printArray(arreglo, size);
+				int pos1=-1;
+				int pos2=-1;
+				int pos3=-1;
+				int pos4=-1;
+				int valuegen=0;
+				int result=0;
+				//Empiezo de intentos
 				while(chances>0){
 					cout<<endl;	
-					cout<<...<<endl<<endl<<"DATA GENERATED"<<endl;
+					cout<<"..."<<endl<<endl<<"DATA GENERATED"<<endl;
 					string cadenauser;
 					cout<<"Ingrese una cadena de 4 numeros (0-9)"<<endl;
 					cin>>cadenauser;
 					int validador2=0;
 					while(validador2==0){
-						
+						int tams= cadenauser.size();
+						if(tams==4){
+							cout<<tams<<" characters.."<<endl<<"Accepted"<<endl;
+							validador2=1;
+						}
+						if(tams>4 || tams<4){
+							cout<<"Out of bounds"<<endl;	
+							cout<<"Ingrese la cadena de nuevo"<<endl;
+							cin>>cadenauser;
+							validador2=0;
+						}					
 					}
-					
-				}
-				freeArray(arreglo);	
+
+					//COMPARACION DE NUMEROS
+					result =0;
+					if(validador2=1){
+						for(int i=0; i<size; i++){
+							valuegen=0;
+							int compare= arreglo[i];
+							for(int j =0; j<4; j++){
+								char number= cadenauser[j];
+								int numberg = number - '0';
+								cout<< numberg<<endl;
+
+								if(numberg == compare && i == j){
+									result=result +1;
+									cout<<"El numero: " << numberg<< " esta en la posicion correcta"<<
+									endl;
+
+									valuegen=1;
+									if(i==0){
+										pos1=1;
+									}
+									if(i==1){
+										pos2=1;
+									}
+
+									if(i==2){
+										pos3=1;
+									}
+									if(i==3){
+										pos4=1;
+									}
+												
+								}
+								if(numberg == compare && valuegen==0){
+									cout<<"El numero: "<<"se ha encontrado el numero dentro del codigo"
+									<<endl;
+								}
+							}//FIN DEL FOR DE LA CADENA DE USUARIO	
+						}//FIN DEL FOR PRINCIPAL
+					}//FIN VALIDADOR
+					chances= chances-1;
+					if(result==4 && pos1==1 && pos2 ==1 && pos3 == 1 && pos4 ==1){
+						cout<<"Ha acertado el codigo"<<endl;
+						for(int i = 0; i<size ; i++){
+							cout<<arreglo[i];
+						}
+						cout<<endl;
+						chances=-1000;
+					}
+					if(chances==0){
+						cout<<"Objective failed"<<endl;
+					}
+				}//FIN WHILE DE TURNOS
+				freeArray(arreglo);
+				arreglo=NULL;	
 			}
 			break;
 			
 			case 2:
 			{
+				cout<<"Iniciando el programa 2"<<endl;
+				int numDecimal=0;
+				cout<<"Ingrese un numero: "<<endl;
+				cin>>numDecimal;
 				
+				if(numDecimal>=0){
+
+				}//FIN IF NUMEROS POSITIVOS
+				if(numDecimal<0){
+
+				}//FIN IF NUMEROS NEGATIVOS
 			}
 			break;
 
@@ -75,11 +165,14 @@ int main(){
 			break;
 
 			default: 
-			
+				cout<<"OUT OF BOUNDS"<<endl;
 			break;
 
 		}
+		cout<<"Desea continuar? 1/ SI 2/NEIN!"<<endl;
+	cin>>respuesta;
 	}
+	cout<<"Thanks for participating!"<<endl;
 	return 0;
 }
 
@@ -140,4 +233,6 @@ void freeArray(int* array){
 	if(array!=NULL){
 		delete[] array;
 	}
+	cout<<endl;
+	cout<<"DATA EXPUNGED"<<endl;
 }
